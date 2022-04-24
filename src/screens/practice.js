76fd54cf883f13004c78edf655e77f1e7f0e81d1
Zainab-Coder <zainab24041999@gmail.com>
2022-04-24 21:57:24 +0500

    second commit

--- src/screens/practice.js
+++ src/screens/practice.js
@@ -6,7 +6,6 @@ export default function Practice() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      //added changes
     </View>
   );
 }
